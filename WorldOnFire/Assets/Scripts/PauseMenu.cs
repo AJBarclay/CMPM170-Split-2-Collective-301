3078059f4f6d5b00b5e6fd3e9d6c489687759acc
JanNing012500 <janing@ucsc.edu>
2021-11-03 15:02:16 -0700

    added basic pause menu

---
+++ WorldOnFire/Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour
+{
+    [SerializeField] private GameObject pauseMenuUI;
+
+    [SerializeField] private bool isPaused;
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            isPaused = !isPaused;
+        }
+
+        if(isPaused)
+        {
+            ActiveMenu();
+        }
+
+        else
+        {
+            DeactivateMenu();
+        }
+    }
+
+    void ActiveMenu()
+    {
+        Time.timeScale = 0;
+        //AudioListener.pause = true; //turns sounds off
+        pauseMenuUI.SetActive(true);
+    }
+
+    public void DeactivateMenu()
+    {
+        Time.timeScale = 1;
+        //AudioListener.pause = true;
+        pauseMenuUI.SetActive(false);
+        isPaused = false;
+    }
+}
